8beb9d7d1e24d7d311d44a72a761c1f8e16fcd30
Brandon Tyler Jardine <brandon.jardine@gmail.com>
2021-04-27 21:38:02 -0500

    add starting message

--- arduinolyzer.ino
+++ arduinolyzer.ino
@@ -1,7 +1,7 @@
 #include "Arduino.h"
 
 const char ADDR[] = { 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52 };
-const int CLOCK = 23;
+const int CLOCK = 2;
 
 void setup() {
   for (int n = 0; n < 16; ++n) {
@@ -12,6 +12,7 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(CLOCK), onClock, RISING);
 
   Serial.begin(57600);
+  Serial.println("Starting...");
 }
 
 void loop() {
